e75c0a7fdbdcf4786224b7d945bc643e4972b8c7
pedro-leoni <pedroleoni1996@gmail.com>
2022-04-12 12:40:29 -0300

    funcion getByQuery & utils para la API OK

--- api/src/controllers/getByQuery.js
+++ api/src/controllers/getByQuery.js
@@ -1,34 +1,35 @@
-const { getAllInfo,getApiInfo,getDbInfo } = require('./utils')
+const { getAllInfo } = require('./utils')
 // [ ] GET /recipes?name="...":
 // Obtener un listado de las recetas que contengan la palabra ingresada como query parameter
 // Si no existe ninguna receta mostrar un mensaje adecuado
 
 
 const getByQuery = async (req, res) => {
-    //const queryName = req.query.name
-    
     try{
+        const queryName = req.query.name.toLowerCase()
+        //console.log('QUERY ->    : ', queryName)
         const allRecipes = await getAllInfo();
-        res.status(200).json({
-            status: 'success',
-            allRecipes: allRecipes.map( e => e)
-        })
-
+        if(queryName) {
+            const queryFilter = await allRecipes.filter( r => r.name.toLowerCase().includes(queryName.toLowerCase()))
+            if(queryFilter.length){
+                res.status(200).json(queryFilter)
+            } else {
+                res.status(404).json({msg: 'Receta no encontrada'})
+            }
+        }
     } catch(err) {
         res.status(404).json({msg: err})
     }
-    // if (queryName){
-    //     const queryFilter = await allRecipes.filter( r => r.name.toLowerCase().includes(queryName.toLowerCase()))
-    //     if(queryFilter.length){
-    //         res.status(200).json(queryFilter)
-    //     } else {
-    //         res.status(404).send('Receta no encontrada')
-    //     }
-    // } else {
-    //     res.status(200).send(allRecipes)
-    // }
-
 }
 
 
 module.exports = getByQuery
+
+
+
+// 
+// const allRecipes = await getAllInfo();
+// res.status(200).json({
+//     status: 'va queriendo',
+//     allRecipes: allRecipes.map( e => e)
+// })
--- api/src/controllers/utils.js
+++ api/src/controllers/utils.js
@@ -1,27 +1,24 @@
-const axios = require('axios')
-// const { APIKEY } = process.env
-const { Recipe, Diet} = require('../db')
+const axios = require('axios');
+const { API_KEY } = process.env;
+const { Recipe, Diet} = require('../db');
 
 
 const getApiInfo = async() => {
-    
-        const apiGet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=&addRecipeInformation=true&apiKey=${process.env.APIKEY}`)
-        const apiInfo = await apiGet.data.results.map( r => {
-            return{
-                id: r.id,
-                name: r.title,
-                resume: r.summary,
-                rate: r.spoonacularScore,
-                healthy_level: r.heathScore,
-                instructions: r.analyzedInstructions.map( i => i.steps ),
-                diet: r.diets.map( d => d),
-                img: r.image
-            }
-        })
-        return apiInfo
-    
-        
-    }
+    const apiGet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=&number=100&addRecipeInformation=true&apiKey=${process.env.API_KEY}`)
+    const apiInfo = await apiGet.data.results.map( r => {
+        return{
+            id: r.id,
+            name: r.title,
+            resume: r.summary,
+            rate: r.spoonacularScore,
+            healthy_level: r.heathScore,
+            instructions: r.analyzedInstructions.map( i => i.steps ),
+            diet: r.diets.map( d => d),
+            img: r.image
+        }
+    })
+    return apiInfo
+}
 
 
 

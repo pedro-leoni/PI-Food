6f9c3837727b085f74df43eb6c96e188edccfdfa
pedro-leoni <pedroleoni1996@gmail.com>
2022-04-21 16:39:02 -0300

    el commit mas esperado, arreglado el objeto que devuelve la db

--- api/src/controllers/utils.js
+++ api/src/controllers/utils.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 require('dotenv').config();
-const { API_KEY, API_KEY1, API_KEY2, API_KEY3, API_KEY4, API_KEY5, API_KEY6, API_KEY7, API_KEY8, API_KEY9 } = process.env;
+const { API_KEY, API_KEY1, API_KEY2, API_KEY3, API_KEY4, API_KEY5 } = process.env;
 const { Recipe, Diet} = require('../db');
 
 
 
 const getApiInfo = async() => {
-    const apiGet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=&number=100&addRecipeInformation=true&apiKey=${API_KEY8}`)
+    const apiGet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=&number=100&addRecipeInformation=true&apiKey=${API_KEY1}`)
     const apiInfo = await apiGet.data.results.map( r => {
         return{
             id: r.id,
@@ -39,13 +39,32 @@ const getDbInfo = async() => {
     })
 }
 
-
+const fixDbInfo = async () => {
+    const fixDbInfo = await Recipe.findAll({include: Diet})
+    const fixedDbInfo = fixDbInfo.map( (e) => {
+        const fixedDiets = []
+        for( let i = 0 ; i < e.dataValues.diets.length ; i++){
+            fixedDiets.push(e.dataValues.diets[i].dataValues.name)
+        }
+        return{
+            id: e.dataValues.id,
+            name: e.dataValues.name,
+            resume: e.dataValues.resume,
+            rate: e.dataValues.rate,
+            healthy_level: e.dataValues.healthy_level,
+            img: e.dataValues.img,
+            diets: fixedDiets
+        }
+    })
+    return fixedDbInfo
+}
 
 
 const getAllInfo = async () => {
     const apiInfo = await getApiInfo()
     //console.log('APIINFO-------------------------------------->\n'+ apiInfo)
-    const dbInfo = await getDbInfo()
+    //const dbInfo = await getDbInfo()
+    const dbInfo = await fixDbInfo()
     //console.log('DBINFO-------------------------------------->\n'+ dbInfo)
     const allInfo = apiInfo.concat(dbInfo)
     //console.log('ALLINFO-------------------------------------->\n'+ allInfo)

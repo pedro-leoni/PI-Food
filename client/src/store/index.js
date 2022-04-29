993b0cec508d51df298aa48df4f483ff59f3613f
pedro-leoni <pedroleoni1996@gmail.com>
2022-04-29 00:48:49 -0300

    probando compose devtools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import  rootReducer  from '../reducers';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = createStore(
     rootReducer,
-    compose( applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store

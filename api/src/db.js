276ada52d572e02fde1cb071b92058a9b2c3852e
pedro-leoni <pedroleoni1996@gmail.com>
2022-05-09 14:26:45 -0300

    Cambios en api/src/db.js: actualice el force true a false

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const { cachedDataVersionTag } = require('v8');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
